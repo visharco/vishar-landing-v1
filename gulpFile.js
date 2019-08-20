6ab6c36a270c075311aafa45edabadf22bca6acf
aras armani <aras.armani2014@gmail.com>
2019-08-20 18:41:02 +0300

    fixed all

--- gulpFile.js
+++ gulpFile.js
@@ -15,9 +15,9 @@ var gulp        = require('gulp'),
  })
 
 
- gulp.task('minify-css', function() {
-    return gulp.src('./src/**/*.css')
-      .pipe(minifyCSS({keepBreaks:true}))
+ gulp.task('minifycss', function() {
+    return gulp.src('./src/index.css')
+    //   .pipe(minifyCSS({keepBreaks:true}))
       .pipe(gulp.dest('./src'))
   });
 
