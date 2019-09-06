0061817386b91b0a83d265afffba8b440bafc1cd
aras armani <aras.armani2014@gmail.com>
2019-09-06 18:52:46 +0300

    class modall is added

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -6,7 +6,7 @@ import HowWorking from '../../component/index/howWorking/howWorking';
 import OurService from '../../component/index/ourService/ourService';
 import SampleDesigns from '../../component/index/sampleDesigns/sampleDesigns';
 import CustomerComment from '../../component/index/customerComment/customerComment';
-
+import './style.css'
 
 
 
@@ -15,11 +15,58 @@ class IndexComponent extends Component {
         super(props);
         this.state = {  }
     }
+    componentDidMount(){
+        this._actionModal('open');
+    }
+
+
+    Modal = React.createRef();
+
+    
+
+    _actionModal = (val) => {
+       
+        
+ 
+
+        if(val === 'open'){
+       
+            const body = document.body;
+            body.style.height = '100vh';
+            body.style.overflowY = 'hidden';
+
+            this.Modal.current.style.display = 'block';
+        }
+        else if (val === 'close'){
+
+            const body = document.body;
+            // const scrollY = body.style.top;
+            body.style.position = '';
+            body.style.top = '';
+            body.style.height = '';
+            body.style.overflowY = '';
+            // window.scrollTo(0, parseInt(scrollY || '0') * -1);
+            // document.getElementById('dialog').classList.remove('show');
+
+            this.Modal.current.style.display = 'none';
+        } 
+        
+
+    }
+
+
     render() { 
         return ( 
             <div>
 
-                
+
+                        <div  ref={this.Modal} className="modal-333" >
+                              <div>
+                                  <div className="close-button" onClick={() => this._actionModal([],'close')}></div>
+                             
+                              </div>
+                        </div>
+
                 <GetStarted />
                 <Categories />
                 <HowWorking />
