a6d23033ad4d108338c51531b393374de05044ec
aras armani <aras.armani2014@gmail.com>
2019-10-06 21:03:37 +0300

    container si fixed

--- src/container/match/match.js
+++ src/container/match/match.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './style.css';
+import rocket from '../../assets/images/rocket.svg'
 
 
 
@@ -9,17 +10,38 @@ class MatchComponent extends Component {
         this.state = {  }
     }
     render() { 
+        const mathcs = (
+             <div className="match-container">
+                 <h1>sdsdsdsd</h1>
+            </div>
+        )
         return ( 
-            <div className=" ">
-                   <div className="container-fluid">
-                       <div className="container">
-                           <div className="match">
-                              
-                            <h1>Match</h1>
-                       
-                           </div>
-                       </div>
-                   </div>
+            <div className="projects" >
+                <div className="P-title" >
+                    <div className="container-fluid">
+                        <div className="container" >
+                            <img src={rocket} alt="راکت" />
+                            <div className="P-title-left">
+                                <h2>در مسابقه شرکت کن</h2>
+                                <h1>با بهترین طرح  خود برنده باش</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="P-body" >
+                    <div className="container-fluid">
+                        <div className="container" >
+                            
+                               {mathcs}
+                               {mathcs}
+                               {mathcs}
+                               {mathcs}
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
          );
     }
